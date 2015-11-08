4830a7a366db53e3e11f4c66affab266c4afe6f0
Matthew Wilson <wormsetc@gmail.com>
2015-11-07 20:52:59 -0500

    Mod server

--- server.js
+++ server.js
@@ -1,16 +1,10 @@
 var express = require('express');
 var app = express();
 
-app.set('port', (process.env.PORT || 5000));
+app.set('port', (5002));
 
-app.use(express.static(__dirname + '/public'));
-
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-
-app.get('/', function(request, response) {
-  response.render('pages/index');
+app.get('/', function(req, res){
+  res.sendFile(__dirname + "/public/index.html");
 });
 
 app.listen(app.get('port'), function() {

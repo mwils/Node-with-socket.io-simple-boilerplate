3f49b314ffc6782c2f950ef8067cab547fb23a4f
Matthew Wilson <wormsetc@gmail.com>
2015-11-09 14:14:39 -0500

    added socket.io

---
+++ readme.md
@@ -0,0 +1,4 @@
+# Socket.io Node simple boilerplate
+
+A very basic Hello World example of Socket.io, but useful to get a quick start for say a hackathon.
+
--- server.js
+++ server.js
@@ -1,10 +1,23 @@
-var express = require('express');
-var app = express();
+// Module dependencies
+var   express = require('express'),
+      app = express(),
+      server = require('http').createServer(app),
+      io = require('socket.io')(server);
 
-var port = process.env.PORT || 5002;
-
+// Set location of static assets
 app.use(express.static(__dirname + '/public'));
 
-app.listen(port, function() {
-  console.log('Node app is running on port', port);
+// Log out to console when a client connects
+io.on('connection', function(client) {
+  console.log('Client connected successfully');
+  client.on('disconnect', function(client) {
+    console.log('Client left');
+  });
+});
+
+
+// Set port or allow Heroku
+var port = process.env.PORT || 8080;
+server.listen(port, function() {
+  console.log('Node app is running on port ', port);
 });
